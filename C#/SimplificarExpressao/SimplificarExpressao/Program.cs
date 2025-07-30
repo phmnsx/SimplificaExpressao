ce93314cece6aff1755fb3e876108a95e51ec90f
HugooPaixao <hs8622436@gmail.com>
2025-07-30 07:38:35 -0300

    Adião da parte 4

--- C#/SimplificarExpressao/SimplificarExpressao/Program.cs
+++ C#/SimplificarExpressao/SimplificarExpressao/Program.cs
@@ -1,10 +1,12 @@
 ﻿using SimplificarExpressao;
+using System.Collections;
+
 
 class Program
 {
     public static void Main()
     {
-        List<int> array = [2,3,5];
+        List<int> array = [2, 3, 5];
         Expression teste = new(array);
         SimplifiedExpression teste2 = new(teste);
         Console.WriteLine("a");
@@ -15,7 +17,22 @@ class Program
         Console.WriteLine("b");
         for (int i = 0; i < teste2.AllExpressions.Count; i++)
         {
-           Console.WriteLine(teste2.AllExpressions[i]); //Da as expressoes q dão verdade com os valores q não importam sendo '-', ou seja, (0100 or 0101) = (010-)
+            Console.WriteLine(teste2.AllExpressions[i]); //Da as expressoes q dão verdade com os valores q não importam sendo '-', ou seja, (0100 or 0101) = (010-)
         }
+        
+       Console.WriteLine("c");
+       ArrayList tabela = Cobertura.GerarTabela(teste2.AllExpressions, teste.Expressions);
+       
+        //Percorre os grupos e imprime o implicante seguido pelos mintermos que ele cobre
+        foreach (ArrayList grupo in tabela)
+        {
+            Console.Write(grupo[0] + " cobre:\n");
+            for (int i = 1; i < grupo.Count; i++)
+            {
+                Console.Write(grupo[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
     }
 }
