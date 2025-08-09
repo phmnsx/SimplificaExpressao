292eeb8cff8dee498c68443519b820eaf485eae5
phmnsx <lynnwastinghertime@gmail.com>
2025-08-09 13:25:50 -0300

    Print e FinalExpression

---
+++ C#/SimplificarExpressao/SimplificarExpressao/FinalExpression.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+
+
+namespace SimplificarExpressao
+{
+	public class FinalExpression
+	{
+		public List<string> Expression { get; }
+
+		public FinalExpression(SimplifiedExpression expression)
+		{
+			List<Termo> termos = new List<Termo>();
+			Expression = new List<string>();
+
+			for(int i = 0; i < expression.AllExpressions.Count; i++)
+			{
+				termos.Add(new Termo(expression.AllExpressions[i]));
+			}
+
+			List<int> TermosContados = new List<int>();
+
+			for(int i = 0; i < termos.Count; i++)
+			{
+                
+                for (int j = 0; j < termos[i].Termos.Count; j++)
+				{
+                    if (!TermosContados.Contains(termos[i].Termos[j]))
+					{
+						this.Expression.Add(termos[i].Expressao);
+						TermosContados.AddRange(termos[i].Termos);
+						break;
+					}
+				}
+			}
+
+		}
+
+		private class Termo
+		{
+			public List<int> Termos { get; set; }
+			public string Expressao;
+			public Termo(string Expressao)
+			{
+				this.Expressao = Expressao;
+				this.Termos = new List<int>();
+				List<string> ExpressoesTemp = new List<string>();
+				ExpressoesTemp.Add(Expressao);
+				do
+				{
+                    List<string> tmpExpressoesTemp = new List<string>();
+					for (int i = 0; i < ExpressoesTemp.Count; i++)
+					{
+						for (int j = 0;  j < ExpressoesTemp[i].Length; j++)
+						{
+							if (ExpressoesTemp[i][j] == '-')
+							{
+								char[] array = ExpressoesTemp[i].ToCharArray();
+								array[j] = '0';
+								tmpExpressoesTemp.Add(new string(array));
+                                array[j] = '1';
+                                tmpExpressoesTemp.Add(new string(array));
+								break;
+							}
+						}
+					}
+					if (tmpExpressoesTemp.Count > 0)
+					{
+						ExpressoesTemp = tmpExpressoesTemp;
+					}
+				}
+				while(hasBadCharacter(ExpressoesTemp));
+
+                for (int i = 0;i < ExpressoesTemp.Count;i++)
+				{
+                    this.Termos.Add(Convert.ToInt32(ExpressoesTemp[i], 2));
+				}
+			}
+
+			private bool hasBadCharacter(List<string >Expressao)
+			{
+				for(int i = 0;i < Expressao.Count;i++)
+				{
+					if (Expressao[i].Contains('-'))
+					{
+						return true;
+					}
+				}
+				return false;
+			}
+		}
+	}
+}
---
+++ C#/SimplificarExpressao/SimplificarExpressao/Print.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SimplificarExpressao
+{ 
+	public static class Print
+	{
+		public static void ExpressaoFinal(FinalExpression expression)
+		{
+			for (int i = 0; i < expression.Expression.Count; i++)
+			{
+				for (int j = 0; j < expression.Expression[i].Length; j++)
+				{
+					if(expression.Expression[i][j] != '-')
+					{
+						if (expression.Expression[i][j] == '0')
+						{
+                            Console.Write("!");
+                        }
+                        Console.Write("v");
+                        Console.Write(j);
+                    }
+				}
+                Console.Write(" + ");
+            }
+		}
+	}
+}
